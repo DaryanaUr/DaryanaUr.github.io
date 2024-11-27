5636e2fbfedaab9d94ee5e64f0c0eb7ab304b810
Daryana Uribe <168155135+DaryanaUr@users.noreply.github.com>
2024-11-27 11:47:44 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Software Developer')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Coding turns imagination into innovation')
+    .pauseFor(2500)
+    .deleteChars(10)
+    .typeString('<strong> our reality!</strong>')
+    .pauseFor(2500)
+    .start();
